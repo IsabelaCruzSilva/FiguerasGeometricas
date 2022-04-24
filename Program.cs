0bd5809b7596c3cf690125d236f7aaae122ef1a4
Isabela Cruz <isabelacruz112@gmail.com>
2022-04-24 13:55:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Quadrado q1 = new Quadrado(10.0);
+
+Console.WriteLine(q1.Lado);
+
+q1.Lado = 8.0;
+
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Retangulo r1 = new Retangulo(10.0, 12.0);
+
+Console.WriteLine(r1.Base);
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro); 
+
+
